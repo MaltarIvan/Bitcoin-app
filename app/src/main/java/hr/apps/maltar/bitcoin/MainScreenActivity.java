30d90efb83f8e6bd27a050b76ebbaae762713432
Ivan Maltar <ivan.maltar@fer.hr>
2017-08-14 18:15:10 +0200

    main screen design

--- app/src/main/java/hr/apps/maltar/bitcoin/MainScreenActivity.java
+++ app/src/main/java/hr/apps/maltar/bitcoin/MainScreenActivity.java
@@ -1,13 +1,40 @@
 package hr.apps.maltar.bitcoin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainScreenActivity extends AppCompatActivity {
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss, dd.MM.yyyy");
+    private TextView timeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        timeTextView = (TextView) findViewById(R.id.time_text_view);
+        setTime();
+
+        Button refreshButton = (Button) findViewById(R.id.refresh_button);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshData();
+            }
+        });
+    }
+
+    private void setTime() {
+        timeTextView.setText(simpleDateFormat.format(new Date()));
+    }
+
+    private void refreshData() {
+        setTime();
     }
 }
